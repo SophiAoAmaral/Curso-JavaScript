e608af7e80267aa1d0670be998b7e8f278432ef3
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-06-25 20:37:01 -0300

    novos exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){//condição simples pois nao tem se nao
+    console.log('Voce ultrapassou a velocidade maxima  MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança')
---
+++ aula11/ex12.js
@@ -0,0 +1,8 @@
+var idade = 4 
+if (idade <18){
+    console.log('criança')
+}else if(idade >=18 && idade <=59){
+        console.log('Adulto')
+    }else{
+        console.log('Idoso')
+    }
